1c4f7d47f347e9b14032f0d389bb39cddeb1767a
evanicela <niyonizigiye.celestin@gmail.com>
2023-03-16 18:58:18 +0300

    first commit

--- shoe/admin.py
+++ shoe/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Shoe
+from .models import Checkout
 
 # Register your models here.
+class ShoeAdmin(admin.ModelAdmin):
+    list_display = ('shoe_name','shoe_description','shoe_price','shoe_image')
+
+class CheckoutAdmin(admin.ModelAdmin):
+    list_display = ('transactionid','amount','phone')
+
+admin.site.register(Shoe,ShoeAdmin)
+admin.site.register(Checkout,CheckoutAdmin)
